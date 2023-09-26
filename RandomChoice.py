827b5bf7f541e6f61b0728adec9dd06a2a534c52
Youssef <youssefelbaye@gmail.com>
2023-09-26 21:51:40 +0200

    adding the main code

---
+++ RandomChoice.py
@@ -0,0 +1,10 @@
+basket = [["Apples", "Bananas"], ["Milk", "Water"]]
+print(basket)
+input("Press enter to change the content ....")
+basket.append(["Cake", "Candy"])
+basket.append(["Orange", "Kiwis"])
+basket.append(["Coffee", "Milk"])
+basket.append([1, 2, 3])
+import random
+
+print(random.choice(basket))
